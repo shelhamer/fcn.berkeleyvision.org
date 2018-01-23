230e02c6c59cff10db9158f21148328a4b7242fb
Evan Shelhamer <shelhamer@imaginarynumber.net>
2018-01-23 13:10:59 -0800

    replace VOC helper with more general visualization utility
    Colorize and visualize outputs by making images instead of relying on
    embedded palettes. The palettes are now made in the PASCAL VOC style.
    
    Note that other datasets can be handled by supplying a palette
    as a no. of classes x 3 array.

---
+++ vis.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+def make_palette(num_classes):
+    """
+    Maps classes to colors in the style of PASCAL VOC.
+    Close values are mapped to far colors for segmentation visualization.
+    See http://host.robots.ox.ac.uk/pascal/VOC/voc2012/index.html#devkit
+
+    Takes:
+        num_classes: the number of classes
+    Gives:
+        palette: the colormap as a k x 3 array of RGB colors
+    """
+    palette = np.zeros((num_classes, 3), dtype=np.uint8)
+    for k in xrange(0, num_classes):
+        label = k
+        i = 0
+        while label:
+            palette[k, 0] |= (((label >> 0) & 1) << (7 - i))
+            palette[k, 1] |= (((label >> 1) & 1) << (7 - i))
+            palette[k, 2] |= (((label >> 2) & 1) << (7 - i))
+            label >>= 3
+            i += 1
+    return palette
+
+def color_seg(seg, palette):
+    """
+    Replace classes with their colors.
+
+    Takes:
+        seg: H x W segmentation image of class IDs
+    Gives:
+        H x W x 3 image of class colors
+    """
+    return palette[seg.flat].reshape(seg.shape + (3,))
+
+def vis_seg(img, seg, palette, alpha=0.5):
+    """
+    Visualize segmentation as an overlay on the image.
+
+    Takes:
+        img: H x W x 3 image in [0, 255]
+        seg: H x W segmentation image of class IDs
+        palette: K x 3 colormap for all classes
+        alpha: opacity of the segmentation in [0, 1]
+    Gives:
+        H x W x 3 image with overlaid segmentation
+    """
+    vis = np.array(img, dtype=np.float32)
+    mask = seg > 0
+    vis[mask] *= 1. - alpha
+    vis[mask] += alpha * palette[seg[mask].flat]
+    vis = vis.astype(np.uint8)
+    return vis
